28825ca29d70077f6400b1040256ea1fede18949
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-06 04:12:30 +0600

    https://webpack.js.org/guides/typescript#using-third-party-libraries

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // 2021-02-06 https://webpack.js.org/guides/typescript#basic-setup
-import * as _ from 'lodash';
+import * as _ from 'lodash'; // 2021-02-06 https://webpack.js.org/guides/typescript#using-third-party-libraries
 function component() {
 	const element = document.createElement('div');
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
